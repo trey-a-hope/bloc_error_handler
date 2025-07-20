e04486cb47278762ad96a935a80166fc716c70a0
Trey Hope <trey.a.hope@gmail.com>
2025-07-20 11:34:31 -0500

    Initial commit of Flutter package 'bloc_error_handler' with essential project files including .gitignore, pubspec.yaml, README.md, and error handling logic in bloc_error_handler.dart. Added metadata and analysis options for project configuration.

---
+++ lib/bloc_error_handler.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grpc/grpc.dart';
+import 'dart:async';
+
+abstract class ErrorState {
+  const ErrorState();
+  String? get error;
+  ErrorState copyWith({String? error});
+}
+
+Future<void> catchError<T extends ErrorState>({
+  required Future<void> Function() action,
+  required Emitter<T> emit,
+  required T state,
+}) async {
+  try {
+    return await action();
+  } on DioException catch (e) {
+    final errorMessage = e.message ?? 'Unknown DioException has occurred';
+    emit(state.copyWith(error: errorMessage) as T);
+  } on GrpcError catch (e) {
+    final errorMessage = e.message ?? 'Unknown GRPC Error: ${e.codeName}';
+    emit(state.copyWith(error: errorMessage) as T);
+  } on TimeoutException catch (e) {
+    final errorMessage = e.message ?? 'Request timed out, please try again';
+    emit(state.copyWith(error: errorMessage) as T);
+  } catch (e) {
+    final errorMessage = 'Unexpected error: ${e.toString()}';
+    emit(state.copyWith(error: errorMessage) as T);
+  }
+}
+
+// class BlocHandler<T extends ErrorState> {
+//   Future<void> handle({
+//     required Future<void> Function() action,
+//     required Emitter<T> emit,
+//     required T state,
+//   }) async {
+//     try {
+//       return await action();
+//     } on DioException catch (e) {
+//       final errorMessage = e.message ?? 'Unknown DioException has occurred';
+//       emit(state.copyWith(error: errorMessage) as T);
+//     } on GrpcError catch (e) {
+//       final errorMessage = e.message ?? 'Unknown GRPC Error: ${e.codeName}';
+//       emit(state.copyWith(error: errorMessage) as T);
+//     } on TimeoutException catch (e) {
+//       final errorMessage = e.message ?? 'Request timed out, please try again';
+//       emit(state.copyWith(error: errorMessage) as T);
+//     } catch (e) {
+//       final errorMessage = 'Unexpected error: ${e.toString()}';
+//       emit(state.copyWith(error: errorMessage) as T);
+//     }
+//   }
+// }
